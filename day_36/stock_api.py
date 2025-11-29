6b7fa6f0a8f338c084c5914ab89b5feb0f3704f5
Zamir29 <zamir.hashorva@gmail.com>
2025-11-29 20:19:11 +0100

    chore: corrected the name of the folders with underscore

---
+++ day_36/stock_api.py
@@ -0,0 +1,35 @@
+import requests
+
+# TODO: define StockAPI class
+# TODO: implement get_closing_prices()
+# TODO: implement get_diff()
+# TODO: error handling
+# TODO: tests
+
+class StockAPI:
+
+    def __init__(self, api_key, ticker, function):
+        self.api_key = api_key
+        self.ticker = ticker
+        self.function = function
+        self.endpoint_url = "https://www.alphavantage.co/query"
+
+    def get_closing_prices(self):
+        """Fetch last 2 closing prices from API"""
+
+        parameters = {
+            "function": self.function,
+            "symbol": self.ticker,
+            "apikey": self.api_key,
+            "outputsize": "compact", # this is the default for Free Members
+            "datatype": "json", # json is the default, alternative csv
+        }
+
+        response = requests.get(url=self.endpoint_url, params=parameters)
+        response.raise_for_status() # error handling, in case it won't work
+
+        return response.json()
+
+    def get_diff(self, price1, price2):
+        """Compute percentage difference"""
+        pass
