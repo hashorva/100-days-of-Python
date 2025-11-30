edc83cced44e5c1992e7af1eb12bd13b33829673
Zamir29 <zamir.hashorva@gmail.com>
2025-11-30 15:58:25 +0100

    feat(messages): added better docstring, improved the body and ready to test

--- day_36/notifications_api.py
+++ day_36/notifications_api.py
@@ -10,6 +10,9 @@ class SendNotifications:
         :param from_n:
         :param to_n:
         :param messages_container: This is a list of dictionaries
+        :param ticker: Stock ticker symbol, e.g. 'TSLA'
+        :param sign: '🔺' or '🔻'
+        :param percentage: numeric percentage move
         """
         self.tw_sid = tw_sid
         self.tw_token = tw_token
@@ -18,15 +21,33 @@ class SendNotifications:
         self.message_container = messages_container
         self.ticker = ticker
         self.sign = sign
-        self.percentage_value = percentage
+        self.percentage_value = round(percentage, 2)
 
     def send_notification(self):
         """
         Take any message and build the body to send to the number defined with to_n
         """
-        pass
+        account_sid = self.tw_sid
+        auth_token = self.tw_token
+        client = Client(account_sid, auth_token)
 
-    def build_body(self):
+        for item in self.message_container:
+            body = self.build_body(article=item)
+            message = client.messages.create(
+                from_=self.from_number,
+                to=self.to_number,
+                media_url=item["media"],
+                body=body
+            )
+
+    def build_body(self, article):
         """
-        Takes the message_container list of dictionaries and returns a body formatted with all the data
+        Takes the article and returns a body formatted with all the data
         """
+        body = (f"{self.ticker}: {self.sign}{self.percentage_value}%\n"
+                f"Headline: {article['headline']}\n"
+                f"Brief: {article['brief']}\n"
+                f"Date: {article['date']}\n"
+                f"Link: {article['url']}")
+
+        return body
