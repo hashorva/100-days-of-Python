d40c1d6d713e1459522a15ea3fc35a7c21187515
Zamir29 <zamir.hashorva@gmail.com>
2025-12-01 20:03:46 +0100

    feat(track): set up the skeleton for the app. Next step build the .env a/ py

--- day_37/main.py
+++ day_37/main.py
@@ -1,7 +1,21 @@
-# Day 37 - Created automatically
+import requests
 
-def main():
-    print('Hello Day 37')
+pixela_endpoint = "https://pixe.la/v1/users"
 
-if __name__ == '__main__':
-    main()
+# Find more info on https://docs.pixe.la/entry/post-user#Request-Body
+user_params = {
+    "token": "",
+    "username": "",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+response = requests.post(url=pixela_endpoint, json=user_params)
+
+print(response.text)
+
+# def main():
+#     print('Hello Day 37')
+#
+# if __name__ == '__main__':
+#     main()
