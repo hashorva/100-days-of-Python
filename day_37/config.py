14361db029b501665d7b96581b26c326e6ef5773
Zamir29 <zamir.hashorva@gmail.com>
2025-12-02 14:12:50 +0100

    added keys to be ready to use in pixe.la graphs

--- day_37/config.py
+++ day_37/config.py
@@ -3,6 +3,13 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
+PIXELA_BASE_URL = "https://pixe.la/v1/users"
+
 PIXELA_TOKEN = os.getenv("PIXELA_TOKEN")
 PIXELA_USERNAME = os.getenv("PIXELA_USERNAME")
 PIXELA_GRAPH_ID = os.getenv("PIXELA_GRAPH_ID")
+
+HABIT_NAME = ""
+HABIT_UNIT = ""
+HABIT_TYPE = ""
+HABIT_COLOR = "" # shibafu (green), momiji (red), sora (blue), ichou (yellow), ajisai (purple) and kuro (black)
