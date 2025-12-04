49046a5e9fa212044ebd3965b33408877ec653ee
Zamir29 <zamir.hashorva@gmail.com>
2025-12-04 19:04:18 +0100

    feat(health): build the config.py and imported Variables in healthz_client.py

--- day_38/config.py
+++ day_38/config.py
@@ -8,3 +8,28 @@ X_APP_URL = "https://app.100daysofpython.dev"
 # Credentials of Nutrition API
 X_APP_ID = os.getenv("X-APP-ID")
 X_APP_KEY = os.getenv("X-APP-KEY")
+
+# API urls
+X_APP_POST = f"{X_APP_URL}/v1/nutrition/natural/exercise"
+X_APP_GET = f"{X_APP_URL}/healthz"
+
+# Error code
+ERROR_CODE = {
+    "401": {
+        "Error Code": "UNAUTHENTICATED",
+        "Description": "Missing or invalid API credentials",
+    },
+    "422": {
+        "Error Code": "INVALID_REQUEST",
+        "Description": "Request validation failed (check required fields)",
+    },
+    "429": {
+        "Error Code": "RATE_LIMIT_EXCEEDED",
+        "Description": "Too many requests (max 60/minute)",
+    },
+    "500": {
+        "Error Code": "INTERNAL_ERROR",
+        "Description": "Server error",
+    },
+
+}
---
+++ day_38/healthz_client.py
@@ -0,0 +1,7 @@
+import datetime
+from config import (
+    X_APP_KEY,
+    X_APP_ID,
+    X_APP_URL,
+
+)
