2db3eb9d8dd58ad4cc7822e19d0e465f621ce767
Zamir29 <zamir.hashorva@gmail.com>
2025-12-04 18:52:21 +0100

    feat(health): Initial build of healthz_client

--- day_38/config.py
+++ day_38/config.py
@@ -2,3 +2,9 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
+
+X_APP_URL = "https://app.100daysofpython.dev"
+
+# Credentials of Nutrition API
+X_APP_ID = os.getenv("X-APP-ID")
+X_APP_KEY = os.getenv("X-APP-KEY")
