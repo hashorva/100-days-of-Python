a5a7645957d55d2241d5dd0c5825f8cac9450c8c
Zamir29 <zamir.hashorva@gmail.com>
2025-12-04 19:21:44 +0100

    feat(health): build the get exercise stats query

--- day_38/healthz_client.py
+++ day_38/healthz_client.py
@@ -1,7 +1,25 @@
-import datetime
+import requests
 from config import (
     X_APP_KEY,
     X_APP_ID,
     X_APP_URL,
-
+    X_APP_GET,
+    X_APP_POST,
+    ERROR_CODE,
 )
+
+def get_exercise_stats():
+    get_query = input("Tell me which exercise you did: ")
+
+    query_params = {
+        "query": get_query,
+    }
+
+    headers = {
+        "x-app-id": X_APP_ID,
+        "x-app-key": X_APP_KEY,
+    }
+
+    response = requests.post(url=X_APP_POST, json=query_params, headers=headers)
+
+    return response
