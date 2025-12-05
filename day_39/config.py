0f1b4780db168b5a5f03ba68c1936a17c600bbc8
Zamir29 <zamir.hashorva@gmail.com>
2025-12-05 19:32:19 +0100

    feat(flight): started data_manager file

--- day_39/config.py
+++ day_39/config.py
@@ -2,3 +2,17 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
+
+# --- SHEETY API ---
+
+# Sheety Token
+SHEETY_KEY = os.getenv("SHEETY_KEY")
+
+# Sheety API key
+SHEETY_BEARER_KEY = f"Bearer {SHEETY_KEY}"
+
+# Sheety API url
+SHEETY_GET = "https://api.sheety.co/3ddb00ef84d1e4724f8af231f008a342/flightDeals/prices"
+SHEET_POST = "https://api.sheety.co/3ddb00ef84d1e4724f8af231f008a342/flightDeals/prices"
+
+# ---
--- day_39/data_manager.py
+++ day_39/data_manager.py
@@ -1,3 +1,31 @@
+import pandas as pd
+import requests
+
+from config import SHEETY_BEARER_KEY, SHEETY_GET, SHEET_POST
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.key = SHEETY_BEARER_KEY
+        self.get_url = SHEETY_GET
+        self.post_url = SHEET_POST
+        self.headers = {"Authorization": SHEETY_BEARER_KEY,}
+
+    def get_table(self):
+
+        response = requests.get(url=self.get_url, headers=self.headers)
+
+        get_data = response.json()
+        worksheet_name = list(get_data.keys())[0]
+
+        get_rows = get_data[f"{worksheet_name}"]
+
+        prices_df = pd.DataFrame.from_dict(get_rows)
+
+        return prices_df, get_rows, response
+
+    def put_to_table(self):
+
+        # need what
+        # need where
+        pass
