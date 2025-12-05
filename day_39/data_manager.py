b518a21749b1844b4efccd0798e17ae8bf6d9473
Zamir29 <zamir.hashorva@gmail.com>
2025-12-05 21:45:45 +0100

    feat(flight): added docstrings and @staticmethod

--- day_39/data_manager.py
+++ day_39/data_manager.py
@@ -3,6 +3,7 @@ import requests
 from config import SHEETY_BEARER_KEY, SHEETY_GET, SHEET_POST
 
 class DataManager:
+    """Handles all the read/write operations to the Google Sheet via Sheety"""
     #This class is responsible for talking to the Google Sheet.
     def __init__(self):
         self.get_url = SHEETY_GET
@@ -11,7 +12,7 @@ class DataManager:
         self.headers = {"Authorization": SHEETY_BEARER_KEY,}
 
     def get_table(self):
-
+        """Return all rows as list of dict [{},{}] and the worksheet name"""
         response = requests.get(url=self.get_url, headers=self.headers)
         response.raise_for_status()
 
@@ -22,24 +23,33 @@ class DataManager:
 
         return get_rows, worksheet_name
 
-    def get_schema(self):
-        rows, _ = self.get_table()
+    @staticmethod
+    def get_schema(rows):
+        """Returns the schema of the Google Sheet"""
         first_row = rows[0]
 
         return list(first_row.keys())
 
     def update_row(self, row_id: int, updates: dict):
+        """
+        Update a single row identified by `row_id` with the given fields in `updates`.
+        Only keys in `updates` are changed.
+        """
         # Build the URL for that row
         row_url = f"{self.put_url}/{row_id}"
 
         # Get the row from Google Sheet
         rows, worksheet_name = self.get_table()
 
+        # Select the row
         row = next(r for r in rows if r["id"] == row_id)
 
         # Edit only the value wit the same key in the row
         for key, value in updates.items():
-            row[key] = value
+            if key in row:
+                row[key] = value
+            else:
+                raise KeyError(f"Column '{key}' doesn't exist in sheet")
 
         # Create the payload to send to Sheety
         payload = {
@@ -53,10 +63,14 @@ class DataManager:
         return update_response
 
     def add_row(self, row_data: dict):
+        """
+        Create a new row using the existing sheet schema.
+        Missing fields are filled with empty strings, 'id' is left for Sheety to assign.
+        """
         # Get worksheet name
-        _,  worksheet_name = self.get_table()
+        rows, worksheet_name = self.get_table()
         # Get the schema
-        schema = self.get_schema()
+        schema = self.get_schema(rows)
 
         # build the new row
         new_row = {}
