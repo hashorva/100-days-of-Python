3f8e2e713a8a0b0b7b043263a3804c449ae206fc
Zamir29 <zamir.hashorva@gmail.com>
2025-12-10 13:49:34 +0100

    feat(flight): initial commit for fligh_data

--- day_39/flight_data.py
+++ day_39/flight_data.py
@@ -1,6 +1,13 @@
 class FlightData:
     #This class is responsible for structuring the flight data.
 
-    def __init__(self):
+    def __init__(self,
+                 price: float,
+                 departure_code: str,
+                 arrival_code: str,
+                 departure_date,
+                 start_date: str,
+                 end_date:str):
+        self.price = price
 
     pass
